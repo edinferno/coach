1967e362c30d6881e90547ea0ffefa3b8ee8a0fb
nao <nao@virtual-nao.(none)>
2014-07-15 22:47:19 +0000

    Searching for the ball now when it is not detected for a while

---
+++ edinferno_coach/include/SPLCoachMessage.hpp
@@ -0,0 +1,29 @@
+#ifndef SPLCOACHMESSAGE_HPP
+#define SPLCOACHMESSAGE_HPP
+
+#include <stdint.h>
+
+#define SPL_COACH_MESSAGE_PORT 3839
+
+#define SPL_COACH_MESSAGE_STRUCT_HEADER "SPLC"
+#define SPL_COACH_MESSAGE_STRUCT_VERSION 2
+#define SPL_COACH_MESSAGE_SIZE 40
+
+struct SPLCoachMessage
+{
+  char header[4]; // "SPLC"
+  uint8_t version; // SPL_COACH_MESSAGE_STRUCT_VERSION
+  uint8_t team; // team number
+
+  // buffer for message
+  uint8_t message[SPL_COACH_MESSAGE_SIZE];
+
+  // constructor
+  SPLCoachMessage()
+  {
+    *(uint32_t*) header = *(const uint32_t*) SPL_COACH_MESSAGE_STRUCT_HEADER;
+    version = SPL_COACH_MESSAGE_STRUCT_VERSION;
+  }
+};
+
+#endif // SPLCOACHMESSAGE_HPP
